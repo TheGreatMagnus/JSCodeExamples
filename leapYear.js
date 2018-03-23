b1e211e991dbdb0373282d88c5a756fcba33c6b1
TheGreatMagnus <daisy@unitingnc.org>
2018-03-23 16:16:41 -0400

    working on space age, added scrabble file

--- leapYear.js
+++ leapYear.js
@@ -1,14 +1,3 @@
-// To find the leap year, below three conditions has to be satisfied,
-//
-// 1. Year should be evenly divisible by 4 (year % 4 == 0)
-// 2. AND (&&) Year should not be evenly divisible by 100 (year % 100 != 0)
-// 3. OR (||) year should be evenly divisible by 400 (Year % 400 == 0)
-//
-// if both the 1st AND 2nd condition is true OR 3rd is true then it is a Leap year.
-
-
-
-
 
 function leapYear(year) {
   if (year % 4 === 0){
--- spaceAge.js
+++ spaceAge.js
@@ -11,3 +11,43 @@
 
 // So if you were told someone were 1,000,000,000 seconds old,
 // you should be able to say that they're 31.69 Earth-years old.
+var age_in_second = x;
+
+31557600 seconds = 0.2408467 mercury years
+31557600 seconds = 1 earth year
+second/earth_seconds * plant_years
+
+function ages_on_planet(seconds) {
+  var seconds = seconds;
+  var earth_seconds = 31557600;
+  var answer = "";
+    switch(planet) {
+      case "Vercury":
+        answer += "You are " + (seconds/earth_seconds)*0.2408467 + " Mercury-years old."
+        break;
+      case "Venus":
+        answer += "You are " + (seconds/earth_seconds)*0.61519726 + " Venus-years old."
+        break;
+    }
+//   switch (expression) {
+// case value1:
+//   //Statements executed when the
+//   //result of expression matches value1
+//   [break;]
+// case value2:
+//   //Statements executed when the
+//   //result of expression matches value2
+//   [break;]
+// ...
+// case valueN:
+//   //Statements executed when the
+//   //result of expression matches valueN
+//   [break;]
+// [default:
+//   //Statements executed when none of
+//   //the values match the value of the expression
+//   [break;]]
+}
+
+
+}
