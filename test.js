a6390ac17ef68d54995f09c0f7957b8a90c2d9f7
TheGreatMagnus <daisy@unitingnc.org>
2018-03-27 23:47:49 -0400

    guessing game directions

--- test.js
+++ test.js
@@ -1,10 +1,27 @@
-var n = 0;
-				while (n <= 3) {
-					console.log("n is now " + n);
-					n++;
-				}
-				
+/*
+ * Programming Quiz: Factorials (4-7)
+ */
 
-// for(var n = 0; n<=3; n++) {
-//   console.log("n is now " + n);
+// your code goes here
+//12! = 12*11*10*9...
+
+for (var x=12; x >= 1; x=x-1){
+  var fact = (x + "!" + " = " + x);
+  var product = x;
+    for (var y=x-1; y >=1; y= y-1) {
+      fact += ("*" +y);
+      product *=y;
+    }
+    console.log(fact + "="+product)
+  }
+
+
 // }
+
+// A factorial is calculated by multiplying a number by all the numbers below it. For instance, 3! or "3 factorial" is 3 * 2 * 1 = 6
+
+// 3! = 3 * 2 * 1 = 6 3!=3∗2∗1=6
+// 4! = 4 * 3 * 2 * 1 = 24 4!=4∗3∗2∗1=24
+// 5! = 5 * 4 * 3 * 2 * 1 = 120 5!=5∗4∗3∗2∗1=120
+
+// Save your final answer in a variable called solution and print it to the console.
