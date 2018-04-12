e6d7ec20ee6ca7c25fe0fd743963588917bb8abf
TheGreatMagnus <daisy@unitingnc.org>
2018-04-11 22:26:57 -0400

    commenting out code

--- ProjectExamples/jadenCase.js
+++ ProjectExamples/jadenCase.js
@@ -13,16 +13,19 @@
 
 
 
-
 function jadenCase(text) {
   var text = document.getElementById('original').value;
   //getting the text from the input field, set it equal to variable text
 
     var newText = text.split(" ").map(function(x){
+      //create a variable newText.  newText is equal to the result of calling the split method on text, and calling the map
+      // method on the resulting array
       return x[0].toUpperCase() + x.slice(1);
+      //return the first letter of each element in the array (x[0])
+      //concatenate it with the rest of the letters in each element (x.slice(1))
     }).join(" ");
+    //turn the array back into a sentence
 
     document.getElementById("editedText").innerHTML = newText;
+    //insert the newText into the document
 };
-
-  //create a variable newText  newText is equal to the result splitting the text into an array separated...
