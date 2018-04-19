3d0dc539f36d4041e32b025277474f78a41b6670
TheGreatMagnus <daisy@unitingnc.org>
2018-04-19 18:12:13 -0400

    adding html example

--- todo/todo.js
+++ todo/todo.js
@@ -1,21 +1,40 @@
 // Adds the completed effect
-$("#to-do-list").on("click", "li", function(){
-	$(this).toggleClass('completed');
-});
 
-// Removing todo
-$("#to-do-list").on("click", "li span", function(e){
-	e.stopPropagation();
-	$(this).parent().fadeOut(function(){
-		$(this).remove();
-	})
-});
+// $("#to-do-list").on("click", "li", function(){
+// 	$(this).toggleClass('completed');
+// });
+//
+// // Removing todo
+// $("#to-do-list").on("click", "li span", function(e){
+// 	e.stopPropagation();
+// 	$(this).parent().fadeOut(function(){
+// 		$(this).remove();
+// 	})
+// });
+//
+// // Adding the todo, using event handler?
+// var nextTask = document.getElementbyId("new-task")
+// nextTask.
+// $("#new-task").keypress(function(e){
+// 	if(e.which === 13){
+// 		var task = $(this).val();
+// 		$(this).val("");
+// 		$("#to-do-list").append("<li> <span class='fa fa-close'></span> " + task + "</li>");
+// 	}
+// })
 
-// Adding the todo
-$("#new-task").keypress(function(e){
-	if(e.which === 13){
-		var task = $(this).val();
-		$(this).val("");
-		$("#to-do-list").append("<li> <span class='fa fa-close'></span> " + task + "</li>");
-	}
-})
+function addItem(text){
+
+  var text = document.getElementById('new-task').value;
+  if (text.length === 0) {
+
+  } else {
+    var newLi = document.createElement('li');
+    var newTask = document.createTextNode(text);
+    newLi.appendChild(newTask);
+    var position = document.getElementsByTagName('ul')[0];
+    position.appendChild(newLi);
+    document.getElementById('new-task').value = '';
+  }
+
+};
